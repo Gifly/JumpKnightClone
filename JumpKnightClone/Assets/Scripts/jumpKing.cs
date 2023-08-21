d6860ac2b8aaa8ea447584fd412c9fca7f50f11a
Gifly <axel.canales@hotmail.com>
2023-08-20 18:34:22 -0600

    Double Jump working

--- JumpKnightClone/Assets/Scripts/jumpKing.cs
+++ JumpKnightClone/Assets/Scripts/jumpKing.cs
@@ -8,8 +8,9 @@ public class jumpKing : MonoBehaviour
     public float walkSpeed;
     public float moveInput;
     public bool isGrounded;
+    public bool isTouchingWall;
     public Rigidbody2D rb;
-    public LayerMask groundMask;
+    public LayerMask groundMask, wallMask;
 
     public PhysicsMaterial2D bounceMat, normalMat;
     public bool canJump = true;
@@ -22,6 +23,10 @@ public class jumpKing : MonoBehaviour
     private Vector2 checkpointPosition;
     private bool hasCheckpoint = false;
 
+    public Vector2 wallJumpForce;
+    public float wallJumpTime = 0.5f;
+    private float wallJumpTimer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,12 +38,23 @@ public class jumpKing : MonoBehaviour
 
         isGrounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - 0.5f), 
             new Vector2(0.9f, 0.4f), 0f, groundMask);
+        isTouchingWall = Physics2D.Raycast(transform.position, Vector2.right * Mathf.Sign(moveInput), 0.7f, wallMask);
         //Debug.Log(rb.velocity.y);
+
+        if (isTouchingWall)
+        {
+            Debug.Log("isTouchingWall");
+        }
+        //if (isGrounded)
+        //{
+        //    Debug.Log("isTouchingGround");
+        //}
         if (jumpValue == 0.0f && isGrounded)
         {
             rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
         }
 
+       
 
         if (jumpValue > 0)
         {
@@ -63,11 +79,17 @@ public class jumpKing : MonoBehaviour
         if (Input.GetKeyDown("space") && canDoubleJump && !isGrounded)
         {
             Debug.Log("Double Jump");
-            Invoke("ResetJump", 0.2f);
+            //Invoke("ResetJump", 0.2f);
 
             rb.velocity = new Vector2(rb.velocity.x, 10.0f);
             canDoubleJump = false;
         }
+
+        /* if(isTouchingWall && Input.GetKeyDown("space"))
+         {
+             rb.velocity = new Vector2(-Mathf.Sign(moveInput) * wallJumpForce.x, wallJumpForce.y);
+             wallJumpTimer = wallJumpTime;
+         }*/
 
 
         if (jumpValue >= 20f && isGrounded)
@@ -98,7 +120,7 @@ public class jumpKing : MonoBehaviour
         if (!hasCheckpoint && jumpDistance >= checkpointThreshold)
         {
             canSetCheckpoint = true;
-            //Debug.Log("Checkpoint");
+            //Debug.Log("Can Checkpoint");
         }
 
         if (Input.GetKeyDown(KeyCode.C) && canSetCheckpoint)
@@ -129,4 +151,39 @@ public class jumpKing : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - 0.5f), new Vector2(0.9f, 0.2f));
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wallMask")
+        {
+            Debug.Log("pared");
+            WallTimer();
+        }
+    }
+
+    void WallTimer()
+    {
+        Debug.Log("WT");
+        if (wallJumpTimer > 0)
+        {
+            wallJumpTimer -= Time.deltaTime;
+            Debug.Log(wallJumpTimer);
+            if (wallJumpTimer <= 0)
+            {
+                rb.velocity = new Vector2(-Mathf.Sign(moveInput) * wallJumpForce.x, wallJumpForce.y);
+                wallJumpTimer = wallJumpTime;
+                Debug.Log("WallJump mamalon");
+            }
+        }
+        
+    }
+        //if (wallJumpTimer >= 0)
+        //{
+        //    //Activar booleano de wallJump
+        //    Debug.Log("Timer XD");
+        //    wallJumpTimer -= Time.deltaTime;
+        //    if (wallJumpTimer >= 0 && wallJump)
+        //    {
+        //        rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -Mathf.Infinity, 0));
+        //    }
+        //}
 }
